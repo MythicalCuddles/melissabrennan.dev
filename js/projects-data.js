645ab55d8adaa9057fab99833b4aedcfa000e338
Melissa Brennan <melissa@melissabrennan.dev>
2026-02-13 17:58:18 +0000

    Amendment to url for DJ sets.

--- js/projects-data.js
+++ js/projects-data.js
@@ -279,7 +279,7 @@ const portfolioData = {
             projects: [
                 {
                     title: '(UK) Happy Hardcore',
-                    url: '#',
+                    url: null,
                     colorClass: 'tile-tone-21',
                     links: [
                         { type: 'link', text: 'Berserk IV - 27th July 2024', url: 'https://youtu.be/qKHb6YY0cPg' },
@@ -288,7 +288,7 @@ const portfolioData = {
                 },
                 {
                     title: 'Trance',
-                    url: '#',
+                    url: null,
                     colorClass: 'tile-tone-22',
                     links: [
                         { type: 'link', text: 'Popup - 27th September 2024', url: 'https://youtu.be/8ZQRPaIT82U' }
